5a608ee6a7292107e3f49e8b283fe1dd29e9ca6d
JinFree <ytrqwe12@gmail.com>
2019-10-06 21:39:10 +0900

    docker script

---
+++ Run_Docker_Container.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+SCRIPT_PATH=$(dirname $(realpath -s $0))
+mkdir $SCRIPT_PATH/openface_data
+docker run -it -v "$SCRIPT_PATH/openface_data:/root/openface/openface_data" -p 9000:9000 -p 8000:8000 bamos/openface /bin/bash
