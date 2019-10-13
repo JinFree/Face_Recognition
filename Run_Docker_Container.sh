2dc7e922dd0e8e9cb1d4a046f3f5483107b0a296
JinFree <ytrqwe12@gmail.com>
2019-10-13 17:47:47 +0900

    edit script for X11 display in docker

--- Run_Docker_Container.sh
+++ Run_Docker_Container.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 SCRIPT_PATH=$(dirname $(realpath -s $0))
+sudo xhost +
+export DISPLAY=:0.0
 mkdir $SCRIPT_PATH/openface_data
-docker run -it -v "$SCRIPT_PATH/openface_data:/root/openface/openface_data" -p 9000:9000 -p 8000:8000 bamos/openface /bin/bash
+docker run -it -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v "/tmp/.X11-unix:/tmp/.X11-unix" -v "$SCRIPT_PATH/openface_data:/root/openface/openface_data" -p 9000:9000 -p 8000:8000 bamos/openface /bin/bash
